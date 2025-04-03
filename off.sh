da9516574e39ae57d4b2df0373b80e578d2ae530
Carlo De Pieri <depieri.carlo@gmail.com>
2025-04-03 19:41:44 +0200

    modify script to turn bluetooth on & off on a Kobo Sage
    also don't load any additional kernel module since they don't seem to be
    needed (nor do they seem to work on the Sage)

--- off.sh
+++ off.sh
@@ -4,5 +4,4 @@ cd "$(dirname "$0")"
 hciconfig hci0 down
 pkill hciattach
 pkill bluetoothd
-rmmod -w /drivers/mx6sll-ntx/wifi/sdio_bt_pwr.ko
-rmmod -w  ./uhid/uhid.ko
+echo "0" >"/sys/devices/platform/bt/rfkill/rfkill0/state"
--- on.sh
+++ on.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-insmod /drivers/mx6sll-ntx/wifi/sdio_bt_pwr.ko
-insmod ./uhid/uhid.ko
-/sbin/hciattach -p ttymxc1 any 1500000 flow -t 20
+# Turn the bluetooth on (on Kobo Sage)
+echo "1" >"/sys/devices/platform/bt/rfkill/rfkill0/state"
+/sbin/hciattach /dev/ttyS1 bcm43xx 1500000 flow -t 20 -b bcm43xx_init
 dbus-send --system --dest=org.bluez --print-reply  /  org.freedesktop.DBus.ObjectManager.GetManagedObjects
 hciconfig hci0 up
