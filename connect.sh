5ce1e0c06901d8127ca636b7aa315d19f83d2c90
Carlo De Pieri <depieri.carlo@gmail.com>
2025-04-03 19:38:58 +0200

    refactor connect script to connect to a device called Q36
    since I can't rely on an fixed address, I need to find out the correct
    one every time

--- connect.sh
+++ connect.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-timeout 5s bluetoothctl connect E4:17:D8:7D:3D:69
+set -e
+
+timeout 5s bluetoothctl power on
+device=$(timeout 5s bluetoothctl devices | grep Q36)
+bluetooth_address=$(echo "$device" | grep -oE '[0-9A-Fa-f]{2}([-:][0-9A-Fa-f]{2}){5}')
+timeout 5s bluetoothctl connect "$bluetooth_address"
